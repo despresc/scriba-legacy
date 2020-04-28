b9e0848c7b201b11707a11d4f2a21401e4163c43
despresc <christian.j.j.despres@gmail.com>
2020-04-28 13:41:31 -0400

    Revert use the use of the generic pretty-show
    The Generic instances for all the types have been kept, but the
    generic-based pretty printer in pretty-show is not fast enough to
    compenasate for the extra instances that need to be defined and
    maintained.

--- test/test-scriba.hs
+++ test/test-scriba.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-import qualified Text.Scriba.Counters          as SC
 import qualified Text.Scriba.Decorate          as SD
 import qualified Text.Scriba.Intermediate      as SI
 import qualified Text.Scriba.Markup            as SM
@@ -11,82 +10,16 @@ import qualified Text.Scriba.Parse             as SP
 import qualified Text.Scriba.Render.Html       as SRH
 
 import           Data.ByteString.Lazy          as BL
-import qualified Data.Map                      as Map
-import qualified Data.Set                      as Set
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as T
 import qualified Data.Text.Lazy                as TL
 import qualified Data.Text.Lazy.Encoding       as TLE
-import           Data.Void                      ( Void
-                                                , absurd
-                                                )
 import           System.FilePath                ( takeFileName )
 import           Test.Tasty
 import           Test.Tasty.Golden
-import qualified Text.Megaparsec               as MP
 import qualified Text.Show.Pretty              as Pretty
 import qualified Text.Blaze.Html.Renderer.Text as HT
-
--- Instances for pretty printing
-instance (Pretty.PrettyVal a, Pretty.PrettyVal b) => Pretty.PrettyVal (Map.Map a b) where
-  prettyVal m = Pretty.Con "fromList" [m']
-    where m' = Pretty.prettyVal $ Map.toList m
-instance Pretty.PrettyVal MP.SourcePos
-instance Pretty.PrettyVal MP.Pos where
-  prettyVal p = Pretty.Con "Pos" [Pretty.prettyVal $ MP.unPos p]
-instance Pretty.PrettyVal i => Pretty.PrettyVal (Set.Set i) where
-  prettyVal m = Pretty.Con "fromList" [m']
-    where m' = Pretty.prettyVal $ Set.toList m
-instance Pretty.PrettyVal Void where
-  prettyVal = absurd
-
-instance Pretty.PrettyVal SP.Doc
-instance Pretty.PrettyVal SP.Attrs
-instance Pretty.PrettyVal SP.SecNode
-
-instance Pretty.PrettyVal SP.SecHeader
-instance Pretty.PrettyVal SP.BlockNode
-instance Pretty.PrettyVal SP.InlineNode
-instance Pretty.PrettyVal SP.InlineContent
-instance (Pretty.PrettyVal t, Pretty.PrettyVal c) => Pretty.PrettyVal (SP.Element t c)
-instance Pretty.PrettyVal SP.BlockContent
-
-instance Pretty.PrettyVal SI.Node
-instance Pretty.PrettyVal SI.Element
-instance Pretty.PrettyVal SI.Meta
-instance Pretty.PrettyVal SI.SourcePresentation
-
-instance Pretty.PrettyVal SC.ContainerName
-instance Pretty.PrettyVal SC.CounterName
-
-instance Pretty.PrettyVal SM.Doc
-instance Pretty.PrettyVal SM.DocAttrs
-instance Pretty.PrettyVal SM.SectionContent
-instance Pretty.PrettyVal i => Pretty.PrettyVal (SM.Title i)
-instance Pretty.PrettyVal SM.TitlingConfig
-instance Pretty.PrettyVal SM.NumberStyle
-instance Pretty.PrettyVal SM.Block
-instance Pretty.PrettyVal SM.Section
-instance Pretty.PrettyVal i => Pretty.PrettyVal (SM.Inline i)
-instance Pretty.PrettyVal SM.FormalConfig
-instance Pretty.PrettyVal SM.SectionConfig
-instance Pretty.PrettyVal SM.Formal
-instance Pretty.PrettyVal SM.Paragraph
-instance Pretty.PrettyVal SM.TitleParts
-instance Pretty.PrettyVal SM.List
-instance Pretty.PrettyVal i => Pretty.PrettyVal (SM.Emph i)
-instance Pretty.PrettyVal i => Pretty.PrettyVal (SM.Quote i)
-instance Pretty.PrettyVal SM.Str
-instance Pretty.PrettyVal SM.InlineMath
-instance Pretty.PrettyVal SM.DisplayMath
-instance Pretty.PrettyVal SM.InlineCode
-instance Pretty.PrettyVal SM.PageMark
-instance Pretty.PrettyVal SM.Varied
-instance Pretty.PrettyVal SM.MixedBlockBody
-instance Pretty.PrettyVal SM.ParContent
-instance Pretty.PrettyVal SM.VariedVar
---
 
 parseOrExplode :: Text -> Text -> SP.Doc
 parseOrExplode name t = case SP.parseDoc' name t of
@@ -99,8 +32,8 @@ markupOrExplode n = case SM.parseDoc n of
   Left  e -> error $ T.unpack $ SM.prettyScribaError e
   Right a -> SD.decorate a
 
-byteShow :: Pretty.PrettyVal a => a -> BL.ByteString
-byteShow = TLE.encodeUtf8 . TL.pack . Pretty.valToStr . Pretty.prettyVal
+byteShow :: Show a => a -> BL.ByteString
+byteShow = TLE.encodeUtf8 . TL.pack . Pretty.ppShow
 
 -- TODO: findByExtension?
 goldenWith
